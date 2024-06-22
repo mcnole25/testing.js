cab0a5dcd9ce6bb2490ca9c11beacc84fdfa531a
mcnole25 <154467369+mcnole25@users.noreply.github.com>
2024-06-22 17:26:03 -0500

    Update testing.js

--- testing.js
+++ testing.js
@@ -66,7 +66,7 @@ ctx.fillStyle = "hsl(" + z22 + ", 100%, 65%)";
 ctx.fill();
 }
 function isPrime(z23) {
-for(i = 2; i < z23; i++)
+for (i = 2; i < z23; i++)
 if (z23 % i === 0) return false;
 return z23 > 1;
 }
